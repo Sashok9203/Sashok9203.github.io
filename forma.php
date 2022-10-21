db2747c2ce6deb56440e54555d729ccb0d419303
Sashok9203 <kovalchuk.olex@gmail.com>
2022-10-21 18:06:04 +0300

    17 - JS-Обробка формии

---
+++ forma.php
@@ -0,0 +1,87 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Сайт web-студії "Web-Deco"</title>
+        
+<style>
+    .shadowtext{
+        text-shadow: 1px 3px 2px white, 0 0 1em red;
+        color : #210042;
+        font-size: 2em;
+    }
+</style>        
+    </head>
+    <body background="images/bg.jpg">
+    <table border="1" align="center" cellpadding="10">
+        <tr>
+            <td background="images/bg-3.jpg" colspan="2" height="150" align="right">
+                <img src="images/logo.png" height="140" width="140" align="left">
+                <font color="Marron"><h1 class="shadowtext">Сайт студії "Web-DECO"</h1></font>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <font size="4"><b>
+                <a href="#">Головна</a>&nbsp;&nbsp;
+                <a href="#">Фотогалерея</a>&nbsp;&nbsp;
+                <a href="#">Телефони</a>&nbsp;&nbsp;
+                <a href="#">Статистика</a>&nbsp;&nbsp;
+                <a href="#">Зареєстровані</a>&nbsp;&nbsp;
+                </b></font>
+            </td>
+        </tr>
+        <tr>
+            <td width="30%" valign="top" >
+            <font size="5" color="navy"><h2>Новини</h2></font>
+                <font size="5">
+                    <ul>
+                     <li><a href="#">Сайт будівельної компанії</a></li>
+                     <li><a href="#">Сайт ТМ "Нова школа"</a></li>
+                     <li><a href="#">Редизайн сайту classno.com.ua</a></li>
+                     <li><a href="#">Розробка CMS для Metro Cash&Carry</a></li>
+                     <li><a href="#">Сайт вызитка дизайнера інтерфейсів</a></li>
+                     <p align="right"><a href="#">інші...</a></p>
+                    </ul>
+                </font>
+                <hr>
+                <h1 align="center"><font color="green">Реєстрація</font></h1>
+                <form action="forma.php" method="post" onsubmit="return send();">
+                <table align="center" bgcolor="#ccc">
+                    <tr>
+                        <td><font color="green">Прізвище</font>: </td>
+                        <td><input type="text" size="10" maxlength="20" name="name2"></td>
+                    </tr>  
+                    <tr>
+                        <td><font color="green">Im'я</font>: </td>
+                        <td><input type="text" size="10" maxlength="20" name="name1"></td>
+                    </tr> 
+                    <tr>
+                        <td><font color="green">E-Mail</font>: </td>
+                        <td><input type="text" size="10" maxlength="20" name="email"></td>
+                    </tr> 
+                    <tr>
+                        <td><font color="green">Пароль</font>: </td>
+                        <td><input type="text" size="10" maxlength="20" name="password"></td>
+                    </tr>    
+                </table>
+                <p align="center">
+                    <input type="submit" value="Зареєструватись">
+                    <input type="reset" value="Очистити">
+                </p>
+                </form>
+                <hr>
+            </td>
+            <td width="70%" ><font size="5" color="navy"><h2>  ФОРМА</h2></font>
+            <font size="5">
+               
+            </font>
+            </td>
+        </tr>
+        <tr>
+            <td background="images/bg-3.jpg" colspan="2" valign="middle" height="90">
+            <font size="4">Сайт розробив Ковальчук Олександр</font>
+            </td>
+        </tr>
+    </table>     
+    </body>
+</html>
